46d05d14d47ef59561fe372cd4feb71dac810375
Alexander Merkulov <sasha@merqlove.ru>
2014-07-01 23:24:07 +0400

    Update _user_configure.rb

--- recipes/_user_configure.rb
+++ recipes/_user_configure.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 node['vsftpd']['users'].each do |user|
-  template "<%= node['vsftpd']['config']['user_config_dir'] %>/<%= user['name'] %>" do
+  template "#{ node['vsftpd']['config']['user_config_dir'] }/#{ user['name'] }" do
     source 'user.erb'
     variables(
       :config => user['config']
