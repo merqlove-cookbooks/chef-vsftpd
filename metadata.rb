4aecf424e183d8b0179f7ac6d303e1dd21567a34
Alexander Merkulov <sasha@merqlove.ru>
2015-03-25 19:53:28 +0300

    Iptables automatic support. Requires manual recipe include.

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,8 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.2.0'
 recipe            'vsftpd::default', 'Installs/configures vsftpd'
 
+depends 'iptables', '~> 0.14.1'
+
 supports 'ubuntu'
 supports 'debian'
 supports 'centos'
---
+++ recipes/_iptables.rb
@@ -0,0 +1,3 @@
+# encoding: utf-8
+
+iptables_rule 'port_vsftpd'
