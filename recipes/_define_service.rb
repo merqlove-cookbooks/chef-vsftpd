42770bcfe322d853c67c97c997d0f42abeea6c2e
Alexander Merkulov <sasha@merqlove.ru>
2018-05-03 02:39:44 +0300

    Bump 0.2.2

--- recipes/_define_service.rb
+++ recipes/_define_service.rb
@@ -1,16 +1,20 @@
 # encoding: utf-8
 
+with_provider = if node['platform'] == 'ubuntu' && node['platform_version'].to_f >= 16.04
+  Chef::Provider::Service::Systemd
+elsif node['platform'] == 'ubuntu' && node['platform_version'].to_f >= 14.04
+  Chef::Provider::Service::Upstart 
+end  
+
 if node['vsftpd']['enabled']
   service 'vsftpd' do
-    provider Chef::Provider::Service::Upstart if node['platform'] == 'ubuntu' &&
-                                                 node['platform_version'].to_f >= 14.04
+    provider with_provider if with_provider
     action [:enable, :start]
     supports :restart => true
   end
 else
   service 'vsftpd' do
-    provider Chef::Provider::Service::Upstart if node['platform'] == 'ubuntu' &&
-                                                 node['platform_version'].to_f >= 14.04
+    provider with_provider if with_provider
     action [:disable, :stop]
     supports :restart => true
   end
