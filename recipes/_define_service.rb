63785ceed1112e168014d0de1cac025de257a34d
Alexander Merkulov <sasha@merqlove.ru>
2018-05-03 02:33:26 +0300

    Update services for newer Ubuntu

--- recipes/_define_service.rb
+++ recipes/_define_service.rb
@@ -3,14 +3,14 @@
 if node['vsftpd']['enabled']
   service 'vsftpd' do
     provider Chef::Provider::Service::Upstart if node['platform'] == 'ubuntu' &&
-      node['platform_version'].to_f >= 14.04
+                                                 node['platform_version'].to_f >= 14.04
     action [:enable, :start]
     supports :restart => true
   end
 else
   service 'vsftpd' do
     provider Chef::Provider::Service::Upstart if node['platform'] == 'ubuntu' &&
-      node['platform_version'].to_f >= 14.04
+                                                 node['platform_version'].to_f >= 14.04
     action [:disable, :stop]
     supports :restart => true
   end
